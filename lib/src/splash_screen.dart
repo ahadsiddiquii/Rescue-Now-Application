25614c9b7b8730cc7c9caec8e1b7acd46e3d823c
ahadsiddiquii <ahad12siddiqui34@gmail.com>
2023-07-24 02:08:17 +0500

    1. Added animation assets 2. Added splash screen assets 3. Added Screen config class 4. Added text widget generic 5. Added text field

--- lib/src/splash_screen.dart
+++ lib/src/splash_screen.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+import 'config/screen_config.dart';
+import 'ui_config/decoration_constants.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    ScreenConfig().init(context);
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SizedBox.expand(
+          child: Padding(
+            padding: EdgeInsets.all(
+              DecorationConstants.kInitScreenHorizontalPadding +
+                  ScreenConfig.screenSizeWidth * 0.1,
+            ),
+            child: Lottie.asset(
+              '${DecorationConstants.animationPath}splash_animation.json',
+              repeat: false,
+            ),
+          ),
+        ));
   }
 }
